7b2defa9b936640ef80a4ca54cbba0122ab7302b
KILLUA <hassanidhmmou@gmail.com>
2022-12-01 01:47:36 +0100

    ps finished

--- ft_ra_rb.c
+++ ft_ra_rb.c
@@ -6,7 +6,7 @@
 /*   By: hidhmmou <hidhmmou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 01:30:56 by hidhmmou          #+#    #+#             */
-/*   Updated: 2022/11/18 21:09:15 by hidhmmou         ###   ########.fr       */
+/*   Updated: 2022/12/01 01:44:42 by hidhmmou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ void	ft_rotate(t_stack **stack)
 	t_stack	*holder;
 	t_stack	*new_tail;
 
-	new_tail = *stack;
-	*stack = (*stack)->next;
-	holder = *stack;
-	while (holder->next)
-		holder = holder->next;
-	holder->next = new_tail;
-	holder->next->next = NULL;
+	if (*stack && (*stack)->next)
+	{
+		new_tail = *stack;
+		*stack = (*stack)->next;
+		holder = *stack;
+		while (holder->next)
+			holder = holder->next;
+		holder->next = new_tail;
+		holder->next->next = NULL;
+	}
 }
 
 void	ft_ra_rb_rr(t_stack **stack_a, t_stack **stack_b, int print, char stack)
--- ft_sa_sb_ss.c
+++ ft_sa_sb_ss.c
@@ -6,7 +6,7 @@
 /*   By: hidhmmou <hidhmmou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 00:41:17 by hidhmmou          #+#    #+#             */
-/*   Updated: 2022/11/18 17:06:24 by hidhmmou         ###   ########.fr       */
+/*   Updated: 2022/12/01 01:33:02 by hidhmmou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ void	ft_swap_stack(t_stack **stack)
 	t_stack	*holder1;
 	t_stack	*holder2;
 
-	holder1 = *stack;
-	holder2 = (*stack)->next;
-	holder1->next = (*stack)->next->next;
-	holder2->next = holder1;
-	*stack = holder2;
+	if (*stack && (*stack)->next)
+	{
+		holder1 = *stack;
+		holder2 = (*stack)->next;
+		holder1->next = (*stack)->next->next;
+		holder2->next = holder1;
+		*stack = holder2;
+	}
 }
 
 void	ft_sa_sb_ss(t_stack **stack_a, t_stack **stack_b, int print, char stack)
